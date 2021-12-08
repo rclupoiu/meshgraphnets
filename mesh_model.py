f0ba39769673fdcc660bfb7cd854e19710637bdc
Robert Lupoiu <rclupoiu@gmail.com>
2021-12-07 20:58:29 -0800

    Fixed dimensioning bug in the decoder MLP

--- mesh_model.py
+++ mesh_model.py
@@ -58,9 +58,9 @@ class MeshGraphNet(torch.nn.Module):
         # decoder: only for node embeddings
         self.decoder = Sequential(Linear( hidden_dim , hidden_dim),
                               ReLU(),
-                              Linear( hidden_dim, output_dim),
+                              Linear( hidden_dim, hidden_dim),
                               ReLU(),
-                              Linear( hidden_dim, hidden_dim)
+                              Linear( hidden_dim, output_dim)
                               )
 
 
